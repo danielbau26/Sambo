41be0979cc7033b9b43a7cd0a0130ef8550ff021
samboni72 <santiagosaco2008@gmail.com>
2025-04-13 09:43:05 -0500

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,34 +1,22 @@
 package model;
 
 import java.util.Random;
-/**
- * Este es el controller principal del juego Battleship, es decir que aca se ahce la "logica" deljuego la cual se llama en el main.
- * 
- */
 
 public class Controller {
-    private Game currentGame;
+    public Game currentGame;
     private Random random;
-    /**
-     * Este es el constructo el cual inicializa el controller del Battleship
-     */
-    //--------------------------------------------------------------------------------------------------------
+
     public Controller() {
         this.random = new Random();
     }
 
-    /**
-     * Crea un juego Normal el cual ya tiene barcos definidos
-     */
     public void createStandardGame() {
         Player human = new Player("Human");
         Player computer = new Player("Computer");
         this.currentGame = new Game(human, computer, true);
         setupComputerShips(true);
     }
- /**
-  * Crea un juego customizado donde tu placeas tus botes
-  */
+
     public void createCustomGame() {
         Player human = new Player("Human");
         Player computer = new Player("Computer");
@@ -36,14 +24,7 @@ public class Controller {
         setupComputerShips(false);
     }
 
-    
-/**
- * Coloca los barcos de la computadora en el tablero.
- * @param isStandard true si es un juego estándar, false si es personalizado.
- * Precondición: debe haber un juego activo.
- * Postcondición: los barcos de la computadora son agregados al tablero.
- */
-    private void setupComputerShips(boolean isStandard) {
+    public void setupComputerShips(boolean isStandard) {
         if (isStandard) {
             addComputerShip(1, "Patrol Boat", false);
             addComputerShip(2, "Destroyer", true);
@@ -56,80 +37,75 @@ public class Controller {
             for (int i = 1; i <= shipCount; i++) {
                 int length = random.nextInt(5) + 1;
                 boolean vertical = random.nextBoolean();
-                addComputerShip(length, "S hip " + i, vertical);
+                addComputerShip(length, "Ship " + i, vertical);
             }
         }
     }
-/**
- * Agrega un barco al tablero de la computadora.
- * @param length tamaño del barco.
- * @param name nombre del barco.
- * @param vertical orientación del barco.
- * Precondición: las coordenadas deben ser válidas y no superponerse con otros barcos.
- * Postcondición: el barco es agregado al tablero de la computadora.
- */
-    private void addComputerShip(int length, String name, boolean vertical) {
-        int x, y;
+
+    public void addComputerShip(int length, String name, boolean vertical) {
+        int x= 0; 
+        int y = 0;
         do {
             x = random.nextInt(10);
             y = random.nextInt(10);
-            
+
             if (vertical && y + length > 10) y = 10 - length;
             if (!vertical && x + length > 10) x = 10 - length;
         } while (!isValidShipPlacement(x, y, length, vertical, currentGame.getComputer()));
-        //
+
         Ship ship = new Ship(name, x, y, length, vertical);
         for (int i = 0; i < length; i++) {
-            int coordX = vertical ? x : x + i;
-            int coordY = vertical ? y + i : y;
+            int coordX;
+            int coordY;
+            if (vertical) {
+                coordX = x;
+                coordY = y + i;
+            } else {
+                coordX = x + i;
+                coordY = y;
+            }
             ship.getCoordinates().add(new Coordinate(coordX, coordY));
+            currentGame.getComputer().getBoard().getGrid()[coordY][coordX] = 1;
         }
         currentGame.getComputer().getBoard().getShips().add(ship);
     }
 
-    /**
- * Agrega un barco al tablero del jugador humano.
- * @param length tamaño del barco.
- * @param vertical orientación del barco.
- * @param x coordenada X inicial.
- * @param y coordenada Y inicial.
- * @param name nombre del barco.
- * Precondición: coordenadas válidas, sin solapamiento.
- * Postcondición: el barco es agregado al tablero del jugador.
- */
     public void addPlayerShip(int length, boolean vertical, int x, int y, String name) {
         Ship ship = new Ship(name, x, y, length, vertical);
         for (int i = 0; i < length; i++) {
-            int coordX = vertical ? x : x + i;
-            int coordY = vertical ? y + i : y;
+            int coordX;
+            int coordY;
+            if (vertical) {
+                coordX = x;
+                coordY = y + i;
+            } else {
+                coordX = x + i;
+                coordY = y;
+            }
             ship.getCoordinates().add(new Coordinate(coordX, coordY));
         }
         currentGame.getHuman().getBoard().getShips().add(ship);
     }
-/**
- * Verifica si una posición es válida para colocar un barco.
- * @param x coordenada X.
- * @param y coordenada Y.
- * @param length longitud del barco.
- * @param vertical orientación.
- * @param player jugador al que pertenece el barco.
- * @return true si es válido, false si no.
- */
+
     public boolean isValidShipPlacement(int x, int y, int length, boolean vertical, Player player) {
         for (int i = 0; i < length; i++) {
-            int checkX = vertical ? x : x + i;
-            int checkY = vertical ? y + i : y;
-            
+            int checkX;
+            int checkY;
+            if (vertical) {
+                checkX = x;
+                checkY = y + i;
+            } else {
+                checkX = x + i;
+                checkY = y;
+            }
+
             if (checkX < 0 || checkX >= 10 || checkY < 0 || checkY >= 10) {
                 return false;
             }
-            
-            for (Ship ship : player.getBoard().getShips())  
-            {
-                for (Coordinate coord : ship.getCoordinates()) 
-                {
-                    if (coord.getX() == checkX && coord.getY() == checkY) 
-                    {
+
+            for (Ship ship : player.getBoard().getShips()) {
+                for (Coordinate coord : ship.getCoordinates()) {
+                    if (coord.getX() == checkX && coord.getY() == checkY) {
                         return false;
                     }
                 }
@@ -138,18 +114,12 @@ public class Controller {
         return true;
     }
 
-    /**
- * El jugador ataca una casilla del tablero enemigo.
- * @param x coordenada X del disparo.
- * @param y coordenada Y del disparo.
- * @return mensaje indicando si fue hit, miss o barco hundido.
- */
     public String playerAttack(int x, int y) {
         Player computer = currentGame.getComputer();
         boolean hit = false;
         boolean sunk = false;
         Ship hitShip = null;
-        
+
         for (Ship ship : computer.getBoard().getShips()) {
             for (Coordinate coord : ship.getCoordinates()) {
                 if (coord.getX() == x && coord.getY() == y) {
@@ -161,12 +131,12 @@ public class Controller {
             }
             if (hit) break;
         }
-        //misse d hit!
+
         if (!hit) {
             computer.getBoard().getGrid()[y][x] = 2;
             return "missed";
         }
-        
+
         sunk = true;
         for (Coordinate coord : hitShip.getCoordinates()) {
             if (computer.getBoard().getGrid()[coord.getY()][coord.getX()] != 2) {
@@ -174,7 +144,7 @@ public class Controller {
                 break;
             }
         }
-        
+
         if (sunk) {
             hitShip.setState(ShipState.SUNKEN);
             for (Coordinate coord : hitShip.getCoordinates()) {
@@ -182,26 +152,23 @@ public class Controller {
             }
             return "---Hit " + hitShip.getName() + " ---sunk";
         }
-        
+
         return "---Hit";
     }
-/**
- * La computadora ataca una casilla del tablero del jugador.
- * @return mensaje indicando si fue hit, miss o barco hundido.
- */
+
     public String computerAttack() {
         Player human = currentGame.getHuman();
         int x, y;
-        
+
         do {
             x = random.nextInt(10);
             y = random.nextInt(10);
         } while (human.getBoard().getGrid()[y][x] == 2 || human.getBoard().getGrid()[y][x] == 3);
-        
+
         boolean hit = false;
         boolean sunk = false;
         Ship hitShip = null;
-        
+
         for (Ship ship : human.getBoard().getShips()) {
             for (Coordinate coord : ship.getCoordinates()) {
                 if (coord.getX() == x && coord.getY() == y) {
@@ -213,12 +180,12 @@ public class Controller {
             }
             if (hit) break;
         }
-        
+
         if (!hit) {
             human.getBoard().getGrid()[y][x] = 2;
             return "Computer attacked [" + x + "," + y + "] --- Miss!";
         }
-        
+
         sunk = true;
         for (Coordinate coord : hitShip.getCoordinates()) {
             if (human.getBoard().getGrid()[coord.getY()][coord.getX()] != 2) {
@@ -226,7 +193,7 @@ public class Controller {
                 break;
             }
         }
-        
+
         if (sunk) {
             hitShip.setState(ShipState.SUNKEN);
             for (Coordinate coord : hitShip.getCoordinates()) {
@@ -234,13 +201,10 @@ public class Controller {
             }
             return "Computer attacked [" + x + "," + y + "] - ---Hit! " + hitShip.getName() + " sunk!";
         }
-        
+
         return "Computer attacked [" + x + "," + y + "] - ---Hit!";
     }
-/**
- * Verifica si el jugador humano ha ganado.
- * @return true si todos los barcos enemigos están hundidos.
- */
+
     public boolean isHumanWinner() {
         for (Ship ship : currentGame.getComputer().getBoard().getShips()) {
             if (ship.getState() != ShipState.SUNKEN) {
@@ -249,11 +213,8 @@ public class Controller {
         }
         return true;
     }
-/**
- * Verifica si la computadora ha ganado.
- * @return true si todos los barcos del jugador están hundidos.
- */
-    public boolean isComputerWinner() {//gethuman
+
+    public boolean isComputerWinner() {
         for (Ship ship : currentGame.getHuman().getBoard().getShips()) {
             if (ship.getState() != ShipState.SUNKEN) {
                 return false;
@@ -261,27 +222,16 @@ public class Controller {
         }
         return true;
     }
-/**
- * Devuelve el tablero del jugador humano en formato texto.
- * @return String con el tablero.
- */
+
     public String getHumanBoard() {
         return formatBoard(currentGame.getHuman().getBoard(), true);
     }
-/**
- * Devuelve el tablero de la computadora en formato texto, ocultando sus barcos.
- * @return String con el tablero.
- */
+
     public String getComputerBoard() {
         return formatBoard(currentGame.getComputer().getBoard(), false);
     }
-/**
- * Formatea el tablero para imprimirlo.
- * @param board tablero a mostrar.
- * @param showShips true si se deben mostrar los barcos.
- * @return String representando el tablero.
- */
-    private String formatBoard(Board board, boolean showShips) {
+
+    public String formatBoard(Board board, boolean showShips) {
         StringBuilder sb = new StringBuilder();
         for (int y = 0; y < 10; y++) {
             for (int x = 0; x < 10; x++) {
@@ -297,10 +247,6 @@ public class Controller {
         return sb.toString();
     }
 
-/**
- * Devuelve la partida actual.
- * @return Game activo.
- */
     public Game getCurrentGame() {
         return currentGame;
     }
--- Main.java
+++ Main.java
@@ -1,270 +1,94 @@
 package ui;
+
 import java.util.Scanner;
 import model.Controller;
-/**
- * Constructor de la clase Main.
- * Inicializa todo.
- */
+
 public class Main {
-    private Controller controller;
-    private Scanner scanner;
-    private int humanWins;
-    private int computerWins;
-    private int standardHumanWins;
-    private int standardComputerWins;
-    private int customHumanWins;
-    private int customComputerWins;
+    private static Controller controller = new Controller();
+    private static Scanner scanner = new Scanner(System.in);
 
-    public Main() {
-        this.controller = new Controller();
-        this.scanner = new Scanner(System.in);
-        this.humanWins = 0;
-        this.computerWins = 0;
-        this.standardHumanWins = 0;
-        this.standardComputerWins = 0;
-        this.customHumanWins = 0;
-        this.customComputerWins = 0;
-    }
-//--------------------------------------------------------------------------------------------------------
     public static void main(String[] args) {
-        Main game = new Main();
-        game.startGame();
-    }
-/**
- * Muestra el menú principal del juego y permite al usuario escoger entre las opciones disponibles.
- */
-    public void startGame() {
-        System.out.println("Welcome to BattleShip 2D simulator");
-        
-        boolean playing = true;
-        while (playing) {
-            System.out.println("\nMain Menu:");
-            System.out.println("1. Play normal game");
-            System.out.println("2. Play custom Game");
-            System.out.println("3. stats");
-            System.out.println("4.  exit");
-            System.out.print("human option: ");
-            
-            int choice = getIntInput(1, 4);
-            
-            switch (choice) {
-                case 1:
-                    playStandardGame();
-                    break;
-                    //--
-                case 2:
-                    playCustomGame();
-                    break;
-                    //--
-                case 3:
-                    showStatistics();
-                    break;
-                    //--
-                case 4:
-                    playing = false;
-                    System.out.println("Thank u for playing");
-                    break;
-            }
-        }
-    }
-/**
- * Inicia una partida estándar con barcos predeterminados.
- * Llama al controlador para crear la partida y luego permite al jugador colocar los barcos.
- */
-    private void playStandardGame() {
-        System.out.println("\nNormal game");
-        controller.createStandardGame();
-        setupHumanShips(true);
-        playGame();
-    }
-/**
- * Inicia una partida personalizada donde el jugador puede editar los barcos.
- * Llama al controlador para crear la partida y gestiona a los barcos personalizados.
- */
+        int option;
 
-    private void playCustomGame() {
-        System.out.println("\nCustom game");
-        controller.createCustomGame();
-        setupHumanShips(false);
-        playGame();
-    }
-/**
- * Permite al jugador humano colocar sus barcos en el tablero.
- * @param isStandard true si es un juego estándar, false si es personalizado.
- */
-    private void setupHumanShips(boolean isStandard) {
-        System.out.println("\nPlace your ships:");
-        
-        if (isStandard) {
-            placeShip(1, "Patrol Boat", false);
-            placeShip(2, "Destroyer xxx", true);
-            placeShip(3, "Submarine", false);
-            placeShip(3, "Cruiser", true);
-            placeShip(4, "Battleship", false);
-            placeShip(5, "Carrier", true);
-        } else {
-            System.out.print("How many ships? (1-10): ");
-            int shipCount = getIntInput(1, 10);
-            
-            for (int i = 1; i <= shipCount; i++) {
-                System.out.println("\nShip #" + i);
-                System.out.print("Length (1-5): ");
-                int length = getIntInput(1, 5);
-                
-                System.out.print("Orientation (1. Vertical / 2. Horizontal): ");
-                int orientation = getIntInput(1, 2);
-                boolean vertical = orientation == 1;
-                
-                System.out.print("Ship name: ");
-                String name = scanner.nextLine();
-                
-                placeShip(length, name, vertical);
+        do {
+            showMenu();  
+            option = scanner.nextInt();  
+
+            switch (option) {
+                case 1:
+                    controller.createStandardGame();
+                    playGame(); 
+                    break;
+                case 2:
+                    controller.createCustomGame();
+                    playGame();  
+                    break;
+                case 3:
+                    System.out.println("Gracias por jugar!");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
             }
-        }
+        } while (option != 3);  
     }
-/**
- * Permite colocar un barco específico del jugador humano en el tablero, validando la posición.
- * @param length longitud del barco.
- * @param name nombre del barco.
- * @param vertical orientación del barco (true si es vertical).
- */
-    private void placeShip(int length, String name, boolean vertical) {
-        boolean placed = false;
-        while (!placed) {
-            System.out.println("\nPlacing " + name + " (" + length + " spaces, " + 
-                              (vertical ? "vertical" : "horizontal") + ")");
-            
-            System.out.print("Start X coordinate (0-9): ");
-            int x = getIntInput(0, 9);
-            
-            System.out.print("Start Y coordinate (0-9): ");
-            int y = getIntInput(0, 9);
-            
-            if (controller.isValidShipPlacement(x, y, length, vertical, controller.getCurrentGame().getHuman())) {
-                controller.addPlayerShip(length, vertical, x, y, name);
-                System.out.println(name + " placed successfully!");
-                placed = true;
-            } else {
-                System.out.println("thats a invalid place.");
-                System.out.println("Try again.");
-                System.out.println("");
-            }
-        }
+
+    
+    public static void showMenu() {
+        System.out.println("menu");
+        System.out.println("1. Jugar partida estandar");
+        System.out.println("2. Jugar partida personalizada");
+        System.out.println("3. salir");
+        System.out.print("Selecciona una opción: ");
     }
-/**
- * Ejecuta el ciclo principal de la partida, alternando entre turnos del jugador y de la computadora.
- * Actualiza las estadísticas al final de la partida.
- */
-    private void playGame() {
-        System.out.println("\n.......");
-        System.out.println("");
-        
+
+    
+    public static void playGame() {
         boolean gameOver = false;
-        boolean humanTurn = true;
-        
+
         while (!gameOver) {
-            if (humanTurn) {
-                humanTurn();
-                if (controller.isHumanWinner()) {
-                    gameOver = true;
-                    humanWins++;
-                    if (controller.getCurrentGame().isStandardGame()) {
-                        standardHumanWins++;
-                    } else {
-                        customHumanWins++;
-                    }
-                    System.out.println("\ncongrats bro U won");
-                    System.out.println("");
-                }
-            } else {
+            
+            System.out.println("Tablero Humano:");
+            System.out.println(controller.getHumanBoard());
+
+            System.out.println("Tablero Computadora:");
+            System.out.println(controller.getComputerBoard());
+
+            // Turno del jugador
+            playerTurn();
+
+            // Turno de la computadora
+            if (!controller.isHumanWinner()) {
                 computerTurn();
-                if (controller.isComputerWinner()) {
-                    gameOver = true;
-                    computerWins++;
-                    if (controller.getCurrentGame().isStandardGame()) {
-                        standardComputerWins++;
-                    } else {
-                        customComputerWins++;
-                    }
-                    System.out.println("\nHell nah, the computer won the game");
-                    System.out.println("");
-                }
             }
-            humanTurn = !humanTurn;
+
+            // verificar ganador
+            if (controller.isHumanWinner()) {
+                System.out.println("¡Felicidades, has ganado!");
+                gameOver = true;
+            } else if (controller.isComputerWinner()) {
+                System.out.println("¡La computadora ha ganado!");
+                gameOver = true;
+            }
         }
     }
-    //----------------------------------------------------------------
-/**
- * tiene el turno del jugador humano.
- */
-    private void humanTurn() {
-        System.out.println("\nPLAYER TURN");
-        System.out.println("ENEMY Board:");
-        System.out.println(controller.getComputerBoard());
-        
-        System.out.print("Enter X coordinate to attack (0-9): ");
-        int x = getIntInput(0, 9);
-        
-        System.out.print("Enter Y coordinate to attack (0-9): ");
-        int y = getIntInput(0, 9);
+
+    //
+    public static void playerTurn() {
+        System.out.println("¡Es tu turno!");
+        System.out.print("Ingresa la coordenada X para disparar: ");
+        int x = scanner.nextInt();
+        System.out.print("Ingresa la coordenada Y para disparar: ");
+        int y = scanner.nextInt();
+
         
         String result = controller.playerAttack(x, y);
-        System.out.println(result);
+        System.out.println("Resultado del ataque: " + result);
     }
-    //----------------------------------------------------------------
-/**
- * tiene el turno del computador.
- */
-    private void computerTurn() {
-        System.out.println("\nCOMPUTERS TURN");
+
+    // turno de la pc
+    public static void computerTurn() {
+        System.out.println("¡Es el turno de la computadora!");
         String result = controller.computerAttack();
         System.out.println(result);
-        
-        System.out.println("\nYour Board:");
-        System.out.println(controller.getHumanBoard());
-    }
-/**
- * Muestra las estadisticas acumuladas
- */
-    private void showStatistics() {
-        System.out.println("\nGAME STATS");
-        System.out.println("Total Wins:");
-        System.out.println("player: " + humanWins);
-        System.out.println("Computer: " + computerWins);
-        
-        System.out.println("\nNormal games:");
-        System.out.println("player: " + standardHumanWins);
-        System.out.println("Computer: " + standardComputerWins);
-        
-        System.out.println("\nCustom Games:");
-        System.out.println("player: " + customHumanWins);
-        System.out.println("Computer: " + customComputerWins);
-    }
-/**
- * solicita un numero int de un valor en especifico.
- * @param min valor mínimo permitido.
- * @param max valor máximo permitido.
- * @return número entero válido introducido por el usuario.
- */
-    private int getIntInput(int min, int max) {
-        while (true) {
-            try {
-                int input = scanner.nextInt();
-                scanner.nextLine();
-                if (input >= min && input <= max) {
-                    return input;
-                } else {
-                    System.out.print("Invalid input. Enter between " + min + "-" + max + ": ");
-                    System.out.println("...");
-                    System.out.println("");
-                }
-                //exeption e para que cualquier accion incorrecta nos tire ese mensaje
-            } catch (Exception e) {
-                System.out.print("Invalid input. Enter a number: ");
-                System.out.println("...");
-                System.out.println("");
-                scanner.nextLine();
-            }
-        }
     }
 }
